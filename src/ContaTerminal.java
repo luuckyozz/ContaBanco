d0dd6b47fb38f65505f64540d9660675e1195ddc
luuckyozz <guilhermealbernaz13@gmail.com>
2024-05-19 21:39:34 -0300

    commit - code ContaTerminal

---
+++ src/ContaTerminal.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner dadosIn = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite o nome do titular: ");
+        String nomeCliente = dadosIn.nextLine();
+
+        // dadosIn.nextLine(); // consumir o \n do nextLine anterior
+
+        System.out.println("Digite o numero da agência: ");
+        String numeroAgencia = dadosIn.nextLine();
+
+        System.out.println("Digite o numero da conta: ");
+        int numeroConta = dadosIn.nextInt();
+
+        System.out.println("Digite o saldo: ");
+        double saldo = dadosIn.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", nomeCliente, numeroAgencia, numeroConta, saldo);
+
+    }
+}
